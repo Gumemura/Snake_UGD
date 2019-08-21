37e16fa84c5af3745a8bfa549f48add422dba5b3
Gumemura <you@example.com>
2019-08-21 01:05:20 -0300

    scrip de movimento

---
+++ Assets/Scenes/Cobra_Mov.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cobra_Mov : MonoBehaviour
+{
+    public int speed = 5;
+
+    private bool canMove = true, moving = false;
+    private Vector3 pos;
+
+    bool mov_up = false;
+    bool mov_down = false;
+    bool mov_left = false;
+    bool mov_right = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow)){
+            mov_up = false;
+            mov_down = true;
+            mov_left = false;
+            mov_right = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow)){
+            mov_up = true;
+            mov_down = false;
+            mov_left = false;
+            mov_right = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            mov_up = false;
+            mov_down = false;
+            mov_left = true;
+            mov_right = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            mov_up = false;
+            mov_down = false;
+            mov_left = false;
+            mov_right = true;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Space)){
+            mov_up = false;
+            mov_down = false;
+            mov_left = false;
+            mov_right = false;
+        }
+
+        if(canMove){
+            pos = transform.position;
+            move();
+        }
+
+        if(moving){
+            if(transform.position == pos){
+                moving = false;
+                canMove = true;
+
+                move();
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+        }
+    }
+
+    private void move(){
+        if(mov_up){
+            canMove = false;
+            moving = true;
+            pos += Vector3.up;
+        }
+        else if (mov_down){
+            canMove = false;
+            moving = true;
+            pos += Vector3.down;
+        }
+        else if(mov_left){
+            canMove = false;
+            moving = true;
+            pos += Vector3.left;
+        }
+        else if (mov_right){
+            canMove = false;
+            moving = true;
+            pos += Vector3.right;
+        }
+    }
+}
---
+++ Assets/Scenes/Spawn_Maca.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Maca : MonoBehaviour
+{
+    public GameObject maca;
+
+    void Start()
+    {
+        InvokeRepeating("GeraMaca", 5, 10);
+    }
+
+    void GeraMaca()
+    {
+        int posX = Random.Range(-8,8);
+        int posY = Random.Range(-4, 4);
+
+        Instantiate(maca, new Vector3(posX, posY, 0), Quaternion.identity);
+    }
+}
