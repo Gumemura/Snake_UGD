381bcccb809b45cfcc609e5a39843b3aa66b0b2a
Gumemura <you@example.com>
2019-11-21 00:49:47 -0300

    ?

---
+++ Assets/Scrips/Cria_Cobra.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cria_Cobra : MonoBehaviour
+{
+    public GameObject maca_contato;
+    public Rigidbody2D corpo_Cobra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(maca_contato.transform.Find("Maca(Clone)").position == transform.position){
+            Instantiate(corpo_Cobra, transform.position, Quaternion.identity, transform); }
+    }
+}
--- Assets/Scrips/Spawn_Maca.cs
+++ Assets/Scrips/Spawn_Maca.cs
@@ -2,36 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Spawn_Maca : MonoBehaviour
 {
     public GameObject maca;
-    bool flag_maca = true;
-    
     public Rigidbody2D cobra_contato;
+    public GameObject parent;
+    bool can_spawn = true;
 
-    void Start(){
-    }
-
-    void Update() {
-        if(flag_maca){
-            Invoke("GeraMaca", 5);
+    void Update(){
+        if(can_spawn){
+            Invoke("GeraMaca", 3);
+            can_spawn = false;
         }
+        PegaContato();
     }
 
-    void GeraMaca()
-    {
+    void GeraMaca(){
         int posX = Random.Range(-8,8);
         int posY = Random.Range(-4, 4);
 
-        flag_maca = false;
-
-        Instantiate(maca, new Vector3(posX, posY, 0), Quaternion.identity);
+        Instantiate(maca, new Vector3(posX, posY, 0), Quaternion.identity, parent.transform);
     }
 
     void PegaContato(){
-        if(cobra_contato.transform.position == maca.transform.position){
-            Debug.Log("MACA E COBRA NO MESMO LUGAR");
-        }
 
+        if(parent.transform.Find("Maca(Clone)").position == cobra_contato.transform.position){
+            foreach (Transform child in gameObject.transform){
+                GameObject.Destroy(child.gameObject);
+            }
+            can_spawn = true;
+        }
     }
 }
